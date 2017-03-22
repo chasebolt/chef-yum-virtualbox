4cfeb0fc840e081068bb487145d941aaa39dbeea
Chase Bolt <chase.bolt@gmail.com>
2017-03-22 13:43:12 -0700

    [ci skip] updating CHANGELOG and bumping metadata

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## v0.1.1
+- releasever requires centos-release pkg, removing this dependency.
+
 ## v0.1.0
 - initial release
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chase Bolt'
 maintainer_email 'chase.bolt@gmail.com'
 license 'Apache 2.0'
 description 'Installs and configures the Virtualbox yum repository'
-version '0.1.0'
+version '0.1.1'
 
 source_url 'https://github.com/chasebolt/chef-yum-virtualbox'
 issues_url 'https://github.com/chasebolt/chef-yum-virtualbox/issues'
